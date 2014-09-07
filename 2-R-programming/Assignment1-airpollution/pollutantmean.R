48b5f62273425c19249612d519c8692b3ed87d90
gunnards <gunnardan@gmail.com>
2014-09-07 14:33:31 +0200

    Syncing

--- 2-R-programming/Assignment1-airpollution/pollutantmean.R
+++ 2-R-programming/Assignment1-airpollution/pollutantmean.R
@@ -1,1 +1,7 @@
-setwd ("~/Documents/Github/datasciencecoursera/2-R-programming/Assignment1-airpollution")
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+        tafla <- data.frame()
+        for (i in id){
+                tafla <- rbind(read.csv(paste(directory,"/",sprintf("%03d",i),".csv", sep="")), tafla)   
+        }       
+        print (round(mean(tafla[[pollutant]], na.rm=TRUE), digits = 3))          
+}
